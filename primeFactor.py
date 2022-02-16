bbfb887f9f2f59833e1e8c47227fe511a710b73a
heenabaheti <98723593+heenabaheti@users.noreply.github.com>
2022-02-16 20:08:12 +0530

    File Uploaded

---
+++ primeFactor.py
@@ -0,0 +1,13 @@
+Number = int(input(" Please Enter any Number: "))
+
+for i in range(2, Number + 1):
+    if (Number % i == 0):
+        isprime = 1
+        for j in range(2, (i // 2 + 1)):
+            if (i % j == 0):
+                isprime = 0
+                break
+
+        if (isprime == 1):
+            print(i, " is a Prime Factor of a Given Number ", Number)
+
