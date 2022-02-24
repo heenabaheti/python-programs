10b7c5db47212733c1336953f117813f41e861b9
heenabaheti <98723593+heenabaheti@users.noreply.github.com>
2022-02-24 19:38:33 +0530

    updated: Binary to decimal and Decimal to binary conversion program

--- junit_testing_programs/binary_to_decimal.py
+++ junit_testing_programs/binary_to_decimal.py
@@ -1,22 +1,36 @@
 def binary_to_decimal(binary_num):
-   n = len(str(binary_num))
-   result = 0
-   for i in range(1, n + 1):
-      result = result + int(binary_num[i - 1]) * 2 ** (n - i)
-   print("The decimal of the given binary is ", result, '.')
-   return result
+      """
+      converting binary number into the decimal number
+      :param binary_num: binary value taking by user
+      :return: decimal converted value
+      """
+      n = len(str(binary_num))
+      # n is storing length of string(binary_num)
+
+      result = 0
+      for i in range(1, n + 1):
+         result = result + int(binary_num[i - 1]) * 2 ** (n - i)
+      print("The decimal of the given binary is ", result, '.')
+      return result
 
 
 def decimal_to_binary(decimal_num):
-   i = 1
-   result = 0
-   while decimal_num > 0:
+    """
+    converting decimal value into the binary value
+    :param decimal_num: decimal value taken by user
+    :return: converting decimal value in binary and returning result
+    """
+    decimal_num = int(decimal_num)
+    i = 1
+    result = 0
+    while decimal_num > 0:
        rem = int(decimal_num % 2)
        result = result + (i * rem)
        decimal_num = int(decimal_num / 2)
        i = i * 10
-   print("The binary of the given number is ", result)
-   return result
+    result = str(result)
+    print("The binary of the given number is ", result)
+    return result
 
 
 def main():
@@ -27,7 +41,7 @@ def main():
       binary_num = input('Enter binary to be converted: ')
       binary_to_decimal(binary_num)
    else:
-      decimal_num = int(input("Enter decimal to be converted: "))
+      decimal_num = input("Enter decimal to be converted: ")
       decimal_to_binary(decimal_num)
 
 
