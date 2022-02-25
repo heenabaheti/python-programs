df13b7e9a111c2cd8febaf564cdbf22c9ec18e33
heenabaheti <98723593+heenabaheti@users.noreply.github.com>
2022-02-25 08:46:22 +0530

    binary search program has beeen added

---
+++ algorithmic_program/binary_search.py
@@ -0,0 +1,40 @@
+def binarySearch(array, search):
+    """
+    program to search a string using binary search tree method
+    :param array: array in which string has to be search
+    :param search: sting which want search
+    :return: position of string where it found
+    """
+
+    position = 0
+    length = len(array)
+    while position <= length:
+        mid = position + ((length - position) // 2)
+
+        res = (search == array[mid])
+
+        # Check if x is present at mid
+        if res == 0:
+            return mid - 1
+
+        # If x greater, ignore left half
+        if res > 0:
+            position = mid + 1
+
+        # If x is smaller, ignore right half
+        else:
+            length = mid - 1
+
+    return -1
+
+
+if __name__ == "__main__":
+
+    arr = ["practice", "makes", "man", "perfect"]
+    x = "man"
+    result = binarySearch(arr, x)
+
+    if result == -1:
+        print("Element not present")
+    else:
+        print("Element found at index", result)
--- junit_testing_programs/test/test_vending_machine.py
+++ junit_testing_programs/test/test_vending_machine.py
@@ -2,8 +2,6 @@
 Tests return change
 """
 from vending_mach import return_change
-from binary_to_decimal import binary_to_decimal
-from decimal_hexadecimal import decimal_to_hex
 
 
 def test_call_with_zero():
@@ -20,10 +18,3 @@ def test_call_with_265():
     """
     assert return_change(265) == [200, 20, 20, 20, 5]
 
-
-def test_binary_to_deci():
-    """
-    when the input is 1111
-    :return: is decimal no 15
-    """
-    assert binary_to_decimal(1111) == 15
